3bb25de30794277e842adbe049d1800825df9df1
Corentin Papot <cpapot@z4r9p7.42lyon.fr>
2023-11-07 17:23:42 +0100

    ok

--- srcs/requirements/mariadb/conf/createDB.sh
+++ srcs/requirements/mariadb/conf/createDB.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
+#db_name = Database Name
+#db_user = User
+#db_pwd = User Password
+
+	echo -e "Creating DataBase"
 	service mysql start;
-	mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';FLUSH PRIVILEGES;"
+	echo "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;" > db1.sql
+	echo "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';" >> db1.sql
+	echo "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' ;" >> db1.sql
+	echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';FLUSH PRIVILEGES;" >> db1.sql
+	echo "FLUSH PRIVILEGES;" >> db1.sql
+	echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$SQL_ROOT_PASSWORD';" >> db1.sql
+
+	mysql < db1.sql
+	echo -e "DataBase created"
+
+	echo -e "Restarting mysql"
 	mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
-	exec mysqld_safe
+
+	echo -e "DataBase restarted"
+	exec "$@"
